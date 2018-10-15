7c5bc88f82873a322704417dc91e07c4f186da87
lixiang <214372150@qq.com>
2018-10-15 14:51:50 +0800

    完善readme文件内容

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# CooCoo
+# CooCoo Package for Microsoft PXT
+
+软件包github地址为：https://github.com/alsrobot-microbit-makecode-packages/CooCoo
+软件包修改维护人员：https://github.com/bobolx
+
+This library provides a Microsoft PXT package for BitBot, see
+https://4tronix.co.uk/bitbot/.
+该软件包专门为micro:bit版CooCoo智能编程机器人开发，控制器详细信息请访问：http://www.alsrobot.cn/goods-874.html
+
+## 让CooCoo行驶    
+使用motorRun()方法设置，CooCoo左右电机的速度数值，是其行驶，速度值得范围是-1023~1023，速度值为正电机正转，速度值为负电机反转
+
+// 向前行驶
+CooCoo.motorRun(1023, 1023)
+
+// 向后行驶
+CooCoo.motorRun(-1023, -1023)
+
+// 向左行驶
+CooCoo.motorRun(-1023, 1023)
+
+// 向右行驶
+CooCoo.motorRun(1023, -1023)
+
+// 停止左电机
+CooCoo.motorStop(MotorDirection.left)
+
+// 停止右电机
+CooCoo.motorStop(MotorDirection.right)
+
+//停止两侧电机
+CooCoo.motorStopAll()
+
+## 让CooCoo发出声音
+
+使用myPlayTone()方法，让CooCoo发出音调，音调包括：do、re、mi、fa、sol、la、si，节拍包括：1/8、1/4、1/2、1、2、4
+
+// 播放音调do节拍为整拍
+CooCoo.myPlayTone(ToneHzTable.do, BeatList.whole_beat)
+
+//播放生日快乐歌
+CooCoo.MyPlayMusic(SongList.birthday)
+
+## 巡线
+
+使用readPatrol()方法实现巡线功能
+
+// 当两个巡线传感器均在黑色跑道上时
+CooCoo.readPatrol(Patrol.black_black)
+
+## 超声波测障碍物距离
+
+使用sensorDistance()方法实现超声波测试障碍物距离的功能，单位可选择μs或cm
+
+//获得障碍物距离（单位为cm）
+CooCoo.sensorDistance(PingUnit.Centimeters)
+
+## 支持
+
+* for PXT/microbit
+
+## License
+
+MIT
---
+++ main.ts
@@ -0,0 +1,246 @@
+
+enum MotorDirection {
+    //% block="左侧"
+    left,
+    //% block="右侧"
+    right
+}
+
+enum ToneHzTable {
+    do = 262,
+    re = 294,
+    mi = 330,
+    fa = 349,
+    sol = 392,
+    la = 440,
+    si = 494
+}
+
+enum BeatList {
+    //% block="1"
+    whole_beat = 10,
+    //% block="1/2"
+    half_beat = 11,
+    //% block="1/4"
+    quarter_beat = 12,
+    //% block="1/8"
+    eighth_beat = 13,
+    //% block="2"
+    double_beat = 14,
+    //% block="4"
+    breve_beat = 15
+}
+
+enum SongList {
+    //% block="生日歌"
+    birthday = 1,
+    //% block="婚礼进行曲"
+    wedding = 2
+}
+
+enum Patrol{
+    //% block="□□"
+    white_white = 1,
+    //% block="□■"
+    white_black = 2,
+    //% block="■□"
+    black_white = 3,
+    //% block="■■"
+    black_black = 4
+}
+
+enum PingUnit {
+    //% block="cm"
+    Centimeters,
+    //% block="μs"
+    MicroSeconds
+}
+
+//% weight=99 icon="\uf0e7" color=#1B80C4
+namespace CooCoo {
+
+
+
+    /**
+     * 设置电机
+     */
+    //% blockId="coocoo_motor" block="电机 左 速度%leftSpeed| 右 速度%rightSpeed"
+    //% leftSpeed.min=-1023 leftSpeed.max=1023
+    //% rightSpeed.min=-1023 rightSpeed.max=1023
+    //% weight=100
+    export function motorRun(leftSpeed: number, rightSpeed: number): void {
+        let leftRotation = 0x0;
+        if(leftSpeed < 0){
+            leftRotation = 0x1;
+        }
+
+        let rightRotation = 0x0;
+        if(rightSpeed < 0){
+            rightRotation = 0x1;
+        }
+        
+       //左电机
+        pins.analogWritePin(AnalogPin.P15, Math.abs(leftSpeed));
+        pins.digitalWritePin(DigitalPin.P12, leftRotation);
+        
+        //右电机
+        pins.analogWritePin(AnalogPin.P1, Math.abs(rightSpeed));
+        pins.digitalWritePin(DigitalPin.P8, rightRotation);
+        
+    }
+
+
+    /**
+     * 停止单个电机
+     */
+    //% blockId="coocoo_stop" block="电机 停止 %direction"
+    //% weight=98
+    export function motorStop(direction: MotorDirection): void {
+        if(direction == MotorDirection.left){
+            pins.analogWritePin(AnalogPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P12, 0);
+        }
+        if(direction == MotorDirection.right){
+            pins.analogWritePin(AnalogPin.P1, 0);
+            pins.digitalWritePin(DigitalPin.P8, 0);
+        }
+    }
+
+
+    /**
+     * 停止所有电机
+     */
+    //% weight=97
+    //% blockId="coocoo_stopAll" block="停止所有电机"
+    export function motorStopAll(): void {
+        //右电机
+        pins.analogWritePin(AnalogPin.P1, 0);
+        pins.digitalWritePin(DigitalPin.P8, 0);
+        //左电机
+        pins.analogWritePin(AnalogPin.P15, 0);
+        pins.digitalWritePin(DigitalPin.P12, 0);
+    }
+
+    /**
+     * 播放音调
+     */
+    //% weight=89
+    //% blockId="coocoo_tone" block="播放音调 %tone| ，节拍 %beatInfo"
+    export function myPlayTone(tone:ToneHzTable, beatInfo:BeatList): void {
+
+        if(beatInfo == BeatList.whole_beat){
+            music.playTone(tone, music.beat(BeatFraction.Whole));
+
+        }
+       
+        if(beatInfo == BeatList.half_beat){
+            music.playTone(tone, music.beat(BeatFraction.Half));
+
+        }
+        
+        if(beatInfo == BeatList.quarter_beat){
+            music.playTone(tone, music.beat(BeatFraction.Quarter));
+
+        }
+
+        if(beatInfo == BeatList.double_beat){
+            music.playTone(tone, music.beat(BeatFraction.Double));
+
+        }
+
+        
+        if(beatInfo == BeatList.eighth_beat){
+            music.playTone(tone, music.beat(BeatFraction.Eighth));
+
+        }
+
+        if(beatInfo == BeatList.breve_beat){
+            music.playTone(tone, music.beat(BeatFraction.Breve));
+
+        }
+        //1、16不行
+        // if(beatInfo == BeatList.sixteen_beat){
+        //     music.playTone(tone, music.beat(BeatFraction.SixTeenth));
+
+        // }
+
+        
+        
+    }
+
+    /**
+     * 播放音乐
+     */
+    //% weight=88
+    //% blockId="coocoo_music" block="播放音乐 %song"
+    export function MyPlayMusic(song: SongList): void {
+        if(song == SongList.wedding){
+            music.beginMelody(music.builtInMelody(Melodies.Wedding), MelodyOptions.Once);
+        }else{
+            music.beginMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once);
+        }
+        
+    }
+
+    //% weight=79
+    //% blockId="coocoo_patrol" block="巡线传感器 %patrol"
+    export function readPatrol(patrol:Patrol): boolean {
+
+        // let p1 = pins.digitalReadPin(DigitalPin.P13);
+        // let p2 = pins.digitalReadPin(DigitalPin.P14);
+
+        if(patrol == Patrol.white_white){
+            if(pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }else if(patrol == Patrol.white_black){
+            if(pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }else if(patrol == Patrol.black_white){
+            if(pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }else if(patrol == Patrol.black_black){
+            if(pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 1){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return true;
+        }
+    }
+
+    //% blockId=coocoo_sensor block="障碍物距离 %unit"
+    //% weight=69
+    export function sensorDistance(unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P2, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(DigitalPin.P2, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(DigitalPin.P2, 0);
+        
+        // read pulse
+        let d = pins.pulseIn(DigitalPin.P2, PulseValue.High, maxCmDistance * 42);
+        //console.log("Distance: " + d/42);
+        
+        basic.pause(50)
+
+        switch (unit) {
+            case PingUnit.Centimeters: return d / 42;
+            default: return d ;
+        }
+    }
+
+}
+
+
