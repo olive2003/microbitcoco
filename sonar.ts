3c8a06ff229ea8a3b613e9877f8f0f4e357e4c48
olive2003 <olive2003@126.com>
2019-07-25 16:20:55 +0800

    fs

--- sonar.ts
+++ sonar.ts
@@ -1,18 +1,26 @@
-
+enum PingUnit {
+    //% block="μs"
+    MicroSeconds,
+    //% block="cm"
+    Centimeters,
+    //% block="inches"
+    Inches
+}
 
 /**
  * Sonar and ping utilities
  */
-//% color="#2c3e50" weight=8
-namespace Banbao {
+//% color="#2c3e50" weight=10
+namespace sonar {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
      * @param trig tigger pin
      * @param echo echo pin
+     * @param unit desired conversion unit
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
-    //% blockId=sonar_ping block="ping trig %trig|echo %echo"
-    export function sonar(trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
         pins.digitalWritePin(trig, 0);
@@ -23,8 +31,10 @@ namespace Banbao {
         // read pulse
         const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
 
-        return Math.idiv(d, 58);
-
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d ;
         }
     }
 }
