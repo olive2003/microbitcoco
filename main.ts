87f1026fbb6ca85a25934a2afb89f0a3f95069a2
olive2003 <olive2003@126.com>
2019-07-26 11:08:07 +0800

    f

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
+enum IOPORT { 
+    A = 1,
+    B = 2,
+    C = 3,
+    D = 4
 
+}
 
 /**
  * Sonar and ping utilities
@@ -7,14 +13,19 @@
 namespace sonar {
     /**
      * Send a ping and get the echo time (in microseconds) as a result
-     * @param trig tigger pin
-     * @param echo echo pin
+     * @param io 在此处描述参数, eg: "C"
+
 
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
     //% blockId=sonar_ping block="ping trig %trig|echo %echo"
-    export function ping(trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
+    export function ping(io: IOPORT,  maxCmDistance = 500): number {
         // send pulse
+        let trig: DigitalPin;
+        let echo: DigitalPin;
+        switch (io) { 
+            case 3: trig = DigitalPin.P5; echo = DigitalPin.P11; break;
+        }
         pins.setPull(trig, PinPullMode.PullNone);
         pins.digitalWritePin(trig, 0);
         control.waitMicros(2);
@@ -23,8 +34,7 @@ namespace sonar {
         pins.digitalWritePin(trig, 0);
         // read pulse
         const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
-
-        return Math.idiv(d, 58);
-
+        return Math.idiv(d,58);
     }
+
 }
