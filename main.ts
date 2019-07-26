5ab6ed242c74c1a56f61ec6ddc5837e40135f89f
olive2003 <olive2003@126.com>
2019-07-26 09:05:56 +0800

    f

--- main.ts
+++ main.ts
@@ -1,11 +1,4 @@
-enum PingUnit {
-    //% block="μs"
-    MicroSeconds,
-    //% block="cm"
-    Centimeters,
-    //% block="inches"
-    Inches
-}
+
 
 /**
  * Sonar and ping utilities
@@ -16,11 +9,11 @@ namespace sonar {
      * Send a ping and get the echo time (in microseconds) as a result
      * @param trig tigger pin
      * @param echo echo pin
-     * @param unit desired conversion unit
+
      * @param maxCmDistance maximum distance in centimeters (default is 500)
      */
-    //% blockId=sonar_ping block="ping trig %trig|echo %echo|unit %unit"
-    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo"
+    export function ping(trig: DigitalPin, echo: DigitalPin, maxCmDistance = 500): number {
         // send pulse
         pins.setPull(trig, PinPullMode.PullNone);
         pins.digitalWritePin(trig, 0);
@@ -31,10 +24,7 @@ namespace sonar {
         // read pulse
         const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
 
-        switch (unit) {
-            case PingUnit.Centimeters: return Math.idiv(d, 58);
-            case PingUnit.Inches: return Math.idiv(d, 148);
-            default: return d ;
-        }
+        return Math.idiv(d, 58);
+
     }
 }
